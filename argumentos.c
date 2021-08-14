0592cd8fd0d82efa25e91a78c53aaa9052e30ad2
Alex AC <alexayca@yahoo.com.mx>
2021-08-13 19:25:20 -0500

    Comenzando y cambio de nombre

---
+++ argumentos.c
@@ -0,0 +1,26 @@
+/* * * * * * * * * * *
+ * Programacion en linux
+ * paso de argumentos
+ * 
+ * * * * * * * * * * * */
+
+int main(int argc, char const *argv[])
+{
+    int tokens;
+
+    for (tokens = 0; tokens < argc; tokens++)
+    {
+        if (argv[tokens][0] == '-')
+        {
+            printf("opcion: %s\n", argv[tokens]+1);
+        }
+        else
+        {
+            printf("argumento %d: %s\n", tokens, argv[tokens]);
+        }
+        
+        
+    }
+    
+    exit (0);
+}
