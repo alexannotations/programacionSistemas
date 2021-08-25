98ccace2cf6884117482f2ea6d773433adf47639
Alex AC <69085537+alexannotations@users.noreply.github.com>
2021-08-25 10:33:03 -0500

    Environment Variables

---
+++ variables.c
@@ -0,0 +1,32 @@
+/* * * * * * * * * * *
+ * Programacion en linux
+ * PID, UID y variables de ambiente
+ * Linux identifica los procesos a partir de un PID,
+ * y los usuarios con UID.
+ * 
+ * Obseve que podemos acceder a información relacionada con nuestro proceso.
+ * Dicha información se encuentra almacenada en un arreglo de cadenas llamado 
+ * variables de ambiente. Accediendo a dicho arreglo podemos utilizar dentro de
+ * nuestro codigo alguna variable que nos interese.
+ * 
+ * * * * * * * * * * * */
+
+#include<stdlib.h>
+#include<stdio.h>
+
+extern char **environ;
+
+int main(int argc, char *argv[])
+{
+    int j;
+
+    printf("Las variables de entorno para %s son:\n", argv[0]);
+
+    for (j = 0; environ[j] != NULL; j++)
+        printf("environ[%d] = %s\n", j, environ[j]);
+    
+    printf("La varible $HOME: %s\n", getenv("HOME"));
+    
+    return 0;
+
+}
